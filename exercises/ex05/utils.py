c0aeeca30f7ddc6bd8a4176c4e6a1a80379893c1
Connor Bassett <connorwa@ad.unc.edu>
2021-10-06 11:24:18 -0400

    end of ex05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,51 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730397999"
 
 # Define your functions below
+
+
+def only_evens(list_of_ints: list[int]) -> list[int]:
+    i: int = 0
+    even_integers: list[int] = list([])
+    while i < len(list_of_ints):
+        if list_of_ints[i] % 2 == 0:
+            even_integers.append(list_of_ints[i])
+            i += 1
+        else:
+            i += 1
+    return even_integers
+
+
+def sub(a_list: list[int], first: int, second: int) -> list[int]:
+    begin_number: int = first
+    end_number: int = second
+    final_list: list[int] = list([])
+    if begin_number < 0:
+        begin_number: int = 0
+    if begin_number > len(a_list):
+        return final_list
+    if end_number < 0:
+        return final_list
+    if end_number > len(a_list):
+        end_number: int = (len(a_list) - 1)
+    if len(a_list) == 0:
+        return final_list
+    first_number: int = a_list[begin_number]
+    second_number: int = a_list[end_number]
+    final_list.append(first_number)
+    final_list.append(second_number)
+    return final_list
+
+
+def concat(first: list[int], second: list[int]) -> list[int]:
+    i: int = 0
+    final_list: list[int] = list()
+    while i < len(first):
+        final_list.append(first[i])
+        i += 1
+    i: int = 0
+    while i < len(second):
+        final_list.append(second[i])
+        i += 1
+    return final_list
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,51 @@
 """Unit tests for list utility functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
+from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730397999"
+
+
+def test_only_evens_positive() -> None:
+    """Tests to make sure single and double digit positive, even integers are returned."""
+    assert only_evens([1, 2, 4, 3, 5, 10, 8]) == [2, 4, 10, 8]
+
+
+def test_only_evens_negative() -> None:
+    """Tests to make sure single and double digit negative, even integers are returned."""
+    assert only_evens([0, -2, -8, -3, -17, -2]) == [0, -2, -8, -2]
+
+
+def test_only_evens_empty_list() -> None:
+    """Tests to make sure an empty list returns an empty list."""
+    assert only_evens([]) == []
+
+
+def test_sub_normal() -> None:
+    """Tests to see if the function works when both integers are within the index range."""
+    assert sub([5, 7, 2, 10, 3], 2, 4) == [2, 3]
+
+
+def test_sub_repeat() -> None:
+    """Tests to see if the function works when all list values are 4."""
+    assert sub([4, 4, 4, 4, 4, 4], 2, 3) == [4, 4]
+
+
+def test_sub_out_of_range() -> None:
+    """Tests to see if the function works when first integer is less than zero and second integer is greater than last index."""
+    assert sub([3, 4, 6, 7, 9, 3], -7, 15) == [3, 3]
+
+
+def test_concat_positive() -> None:
+    """Tests to see if the function works when both lists have positive integers."""
+    assert concat([3, 4, 6, 7, 9, 3], [4, 3, 2, 9, 10]) == [3, 4, 6, 7, 9, 3, 4, 3, 2, 9, 10]
+
+
+def test_concat_negative() -> None:
+    """Tests to see if the function works when both lists have negative integers."""
+    assert concat([-3, -4, -6, -7, -9, -3], [-4, -3, -2, -9, -10]) == [-3, -4, -6, -7, -9, -3, -4, -3, -2, -9, -10]
+
+
+def test_concat_empty() -> None:
+    """Tests to see if the function works when both lists are empty."""
+    assert concat([], []) == []
